d20361c22781f389b8d37561fdbf49ca5ec37cba
Adam Żbikowski <ajzbikowski@gmail.com>
2023-05-27 02:39:01 +0100

    Add uploader, playlist view count, video count

--- src/lib/Playlist.svelte
+++ src/lib/Playlist.svelte
@@ -1,39 +1,52 @@
 <script lang="ts">
-  export let json: any;
-  let thumbnail = json?.thumbnails?.[json?.thumbnails.length - 1 || 0];
+	export let json: any;
+	let thumbnail = json?.thumbnails?.[json?.thumbnails.length - 1 || 0];
 
-  function defaultThumbnail(thumbnails: any) {
-    return thumbnails[thumbnails.length - 1 || 0].url;
-  }
+	function defaultThumbnail(thumbnails: any) {
+		return thumbnails[thumbnails.length - 1 || 0].url;
+	}
 </script>
 
-<h2 class="h2 mb-6">{json.title}</h2>
-<div class="entries">
-  {#each json.entries as { title, channel, thumbnails }}
-    <div class="entry">
-      <div class="thumbnail" style="background-image: url('{defaultThumbnail(thumbnails)}')"></div>
-      <div class="flex flex-col ml-4">  
-        <div class="">{title}</div>
-        <div class="text-gray-800">{channel || ""}</div>
-      </div>
+<div class="playlist">
+	<div class="card px-4 py-4 flex flex-col">
+    <div class="thumbnail !w-full mb-3" style="background-image: url('{defaultThumbnail(json.thumbnails)}')">
     </div>
-  {/each}
+		<h2 class="h2 mb-3 text-xl font-bold opacity-90">{json.title}</h2>
+    <p>Playlist by <a href={json.uploader_url}>{json.uploader}</a></p>
+		<p>
+      {json.playlist_count} videos
+      &bull;
+      {Intl.NumberFormat().format(json.view_count)} views
+    </p>
+		<p>{json.description || ''}</p>
+	</div>
+	<div class="entries">
+		{#each json.entries as { title, channel, thumbnails }}
+			<div class="entry">
+				<div class="thumbnail" style="background-image: url('{defaultThumbnail(thumbnails)}')" />
+				<div class="flex flex-col justify-center ml-4">
+					<div class="">{title}</div>
+					<div class="text-gray-800">{channel || ''}</div>
+				</div>
+			</div>
+		{/each}
+	</div>
 </div>
 
-<style>
-  .entries {
-    @apply flex flex-col gap-2;
-  }
-  
-  .entry {
-    @apply flex;
+<style lang="postcss">
+  .playlist {
+    @apply grid gap-6 grid-cols-1 lg:grid-cols-[auto_1fr];
   }
 
-  .entry .thumbnail {
-    @apply w-24 aspect-video bg-center bg-cover rounded-md;
-  }
-  
-  .entry img {
-    @apply w-full;
-  }
+	.entries {
+		@apply flex flex-col gap-2;
+	}
+
+	.entry {
+		@apply flex;
+	}
+
+	.thumbnail {
+		@apply w-24 aspect-video bg-center bg-cover rounded-md;
+	}
 </style>
