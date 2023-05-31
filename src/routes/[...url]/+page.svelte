1c54a322774d28b7208a144ce9f53c593b6eea3c
Adam Żbikowski <ajzbikowski@gmail.com>
2023-05-31 05:40:10 +0100

    Create page

---
+++ src/routes/[...url]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import BigVideo from "$lib/BigVideo.svelte";
+import Playlist from "$lib/Playlist.svelte";
+  import Video from "$lib/Video.svelte";
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+  let { videoId, playlistId, json } = data;
+  $: ({ videoId, playlistId, json } = data);
+
+  $: console.log(json);
+</script>
+
+{#if playlistId}
+  <Playlist {json} />
+{:else if videoId}
+  <BigVideo {json} />
+{/if}
