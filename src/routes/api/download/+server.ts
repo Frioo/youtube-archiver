a3b357262b4ae8f70340087a900164f93cf1bd75
Adam Żbikowski <ajzbikowski@gmail.com>
2023-05-31 09:50:53 +0100

    Cleanup

--- src/routes/api/download/+server.ts
+++ src/routes/api/download/+server.ts
@@ -1,5 +1,4 @@
 import { error } from '@sveltejs/kit';
-import https from 'node:https';
 import type { RequestHandler } from './$types';
 
 export async function GET({ url }): Promise<RequestHandler> {
@@ -9,8 +8,8 @@ export async function GET({ url }): Promise<RequestHandler> {
 	}
 	const videoUrl = new URL(val);
 	const mime = videoUrl.searchParams.get('mime');
-
 	const res = await fetch(videoUrl);
+
 	return new Response(res.body, {
 		headers: {
 			'Content-Type': mime
