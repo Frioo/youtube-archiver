43d87bde068971d38adf5eb3bca1a278eb44f804
Adam Żbikowski <ajzbikowski@gmail.com>
2023-05-31 08:05:05 +0100

    Add download options

--- src/lib/BigVideo.svelte
+++ src/lib/BigVideo.svelte
@@ -3,11 +3,21 @@
   import dayjs from "dayjs";
   import dayjsDuration from "dayjs/plugin/duration";
   import dayjsRelativeTime from "dayjs/plugin/relativeTime";
+	import { DownloadIcon, ExternalLinkIcon, EyeIcon, FileIcon, HeadphonesIcon, VideoIcon } from "svelte-feather-icons";
   dayjs.extend(dayjsDuration);
   dayjs.extend(dayjsRelativeTime);
 
   export let json: any;
-  let { title, description, thumbnails, view_count, duration, channel, channel_url, } = json;
+  let { 
+    title,
+    description, 
+    thumbnails, 
+    view_count, 
+    duration, 
+    channel, 
+    channel_url, 
+    formats,
+  } = json;
 
   function durationText(seconds: number) {
     const dur = dayjs.duration(seconds, 's');
@@ -21,19 +31,91 @@
   }
 </script>
 
-<div class="card m-4 w-min overflow-hidden">
-  <div class="relative isolate">
-    <img class="max-w-sm" src={defaultThumbnail(thumbnails)} alt="">
-    <span class="badge variant-filled absolute bottom-1 right-1">{durationText(duration)}</span>
-  </div>
-  <div class="p-4">
-    <h2 class="h2 text-xl font-medium">{title}</h2>
-    <div class="flex mt-2 mb-4 gap-2">
-      <a href={channel_url} target="_blank" class="badge variant-soft transition-colors duration-100 hover:variant-filled-primary">{channel}</a>
-      <span class="badge variant-soft">{Intl.NumberFormat().format(view_count)} views</span>
+<div class="card m-4 overflow-hidden">
+  <div class="flex">
+    <!-- <div class="">
+      <img class="w-80 h-auto" src={defaultThumbnail(thumbnails)} alt="">
+    </div> -->
+    <div class="thumbnail rounded-none relative w-80 h-fit" style="background-image: url('{defaultThumbnail(thumbnails)}')">
+      <span class="badge variant-filled absolute bottom-1 right-1">{durationText(duration)}</span>
     </div>
-    <p class="p whitespace-pre-line">
-      {description}
-    </p>
+    <div class="flex flex-col w-fit p-4">
+      <h2 class="h2 text-xl font-medium">{title}</h2>
+      <div class="flex mt-2.5 mb-4 gap-2">
+        <a href={channel_url} target="_blank" class="badge variant-soft transition-colors duration-100 hover:variant-filled-primary">
+          <ExternalLinkIcon size="1.25x" class="mr-1.5" />
+          {channel}
+        </a>
+        <span class="badge variant-soft">
+          <EyeIcon size="1.25x" class="mr-1.5" />
+          {Intl.NumberFormat().format(view_count)} views
+        </span>
+      </div>
+
+      
+      <!-- <p class="p whitespace-pre-line">
+        {description}
+      </p> -->
+    </div>
+  </div>
+</div>
+
+<div class="grid grid-cols-2 gap-8">
+  <div class="m-4 flex flex-col gap-2 w-full">
+    <h2 class="h2 text-xl font-medium my-2">Video Only</h2>
+    {#each formats.filter((f) => f.acodec === 'none' && f.vcodec !== "none") as f}
+      <div class="card px-4 py-2.5 flex gap-2 items-center">
+        {f.format_note}{f.fps}
+        <span class="badge variant-soft">
+          {f.ext}
+        </span>
+        <span class="badge variant-soft">
+          <HeadphonesIcon size="1.25x" class="mr-2" />
+          {f.acodec}
+        </span>
+        <!-- <span class="badge variant-soft">
+        </span> -->
+        <div class="badge variant-soft">
+          <VideoIcon size="1x" class="mr-2" />
+          {f.vcodec}
+        </div>
+
+        <div class="grow w-8"></div>
+
+        <a href={f.url} target="_blank" class="btn btn-sm variant-filled-primary">
+          <DownloadIcon size="1.25x" class="mr-2" />
+          Download
+        </a>
+      </div>
+    {/each}
+  </div>
+
+  <div class="m-4 flex flex-col gap-2 w-fit">
+    <h2 class="h2 text-xl font-medium my-2">Quick Download</h2>
+    {#each formats.filter((f) => f.acodec !== 'none' && f.vcodec !== "none") as f}
+      <div class="card px-4 py-2.5 flex gap-2 items-center">
+        {f.format_note}{f.fps}
+        <span class="badge variant-soft">
+          {f.ext}
+        </span>
+        <span class="badge variant-soft">
+          <HeadphonesIcon size="1.25x" class="mr-2" />
+          {f.acodec}
+        </span>
+        <!-- <span class="badge variant-soft">
+        </span> -->
+        <div class="badge variant-soft">
+          <VideoIcon size="1x" class="mr-2" />
+          {f.vcodec}
+        </div>
+
+        <div class="grow w-8"></div>
+
+        <a href={f.url} target="_blank" class="btn btn-sm variant-filled-primary">
+          <DownloadIcon size="1.25x" class="mr-2" />
+          Download
+        </a>
+      </div>
+    {/each}
   </div>
 </div>
