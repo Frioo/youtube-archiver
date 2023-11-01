a682afa976d586c5a595c788be949e3328ad2aa7
Adam Żbikowski <ajzbikowski@gmail.com>
2023-11-01 02:00:43 +0000

    List archived videos

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import Playlist from "$lib/Playlist.svelte";
   import { toastStore, ProgressBar } from "@skeletonlabs/skeleton";
+	import type { PageData } from "./$types";
+	import { PUBLIC_POCKETBASE_URL } from "$env/static/public";
+	import { invalidateAll } from "$app/navigation";
+
+  export let data: PageData;
+  let { videos } = data;
+  $: ({ videos } = data);
 
   let playlists: string[] = [];
   let url: string = "";
@@ -35,7 +42,17 @@
   }
 
   async function handleVideo() {
+    const res = await fetch('/api/save', {
+      method: 'POST',
+      body: JSON.stringify({ url }),
+    });
 
+    if (res.status !== 200) {
+      console.log(await res.json());
+      return;
+    }
+
+    invalidateAll();
   }
 </script>
 
@@ -62,7 +79,7 @@
       type="url"
       name="url"
       placeholder="Video URL">
-    <button class="variant-filled" on:click={handleVideo}>
+    <button class="variant-filled" on:click={() => handleVideo()}>
       Fetch
     </button>
   </div>
@@ -78,4 +95,19 @@
   {#if json}
     <Playlist {json} />
   {/if}
+</div>
+
+<h2 class="text-3xl font-semibold px-4 mb-2">Your videos</h2>
+<div class="grid grid-cols-3 gap-4 p-4">
+  {#each videos as { title, video_id, thumbnail, id, collectionId, expand }}
+    <div class="flex gap-x-2">
+      <div class="rounded-sm aspect-[16/9] bg-cover bg-center" style="background-image: url('{PUBLIC_POCKETBASE_URL}/api/files/{collectionId}/{id}/{thumbnail}')">
+        <div class="w-48"></div>
+      </div>
+      <div class="">
+        <span class="inline-block font-semibold leading-snug mb-1">{title}</span>
+        <a href={expand?.channel?.handle_url} target="_blank" class="block hover:text-blue-700">{expand?.channel?.name}</a>
+      </div>
+    </div>
+  {/each}
 </div>
