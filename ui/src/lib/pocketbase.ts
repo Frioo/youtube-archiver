dcbd935f92f3f066251f354c0a7fcf6d8fa6d934
Adam Żbikowski <ajzbikowski@gmail.com>
2023-11-01 02:00:59 +0000

    Add pocketbase env var

--- ui/src/lib/pocketbase.ts
+++ ui/src/lib/pocketbase.ts
@@ -1,5 +1,6 @@
 import PocketBase from 'pocketbase';
+import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 
-const pocketbase = new PocketBase('http://localhost:8090');
+const pocketbase = new PocketBase(PUBLIC_POCKETBASE_URL);
 
 export default pocketbase;
