3a543dd826569d6a644e7202d456e604cd38e885
Adam Żbikowski <ajzbikowski@gmail.com>
2023-11-07 00:34:05 +0000

    Refactor URL parsing

--- ui/src/lib/URLInput.svelte
+++ ui/src/lib/URLInput.svelte
@@ -1,15 +1,11 @@
 <script context="module" lang="ts">
-  export interface YTUrl {
-    url: string
-    videoId?: string
-    playlistId?: string
-    index?: number
-    t?: number
-  }
+  
 </script>
 
 <script lang="ts">
 	import { createEventDispatcher } from "svelte";
+	import type { YTUrl } from "$lib/model/YTUrl";
+	import { parseUrl } from "$lib/utils";
 
   export let url: YTUrl | undefined = undefined;
   let _url: string;
@@ -17,26 +13,7 @@
   const dispatch = createEventDispatcher();
 
   async function handleUrl() {
-    const parsed = new URL(_url);
-    if (!parsed) {
-      // invalid url
-    }
-
-    const { hostname, pathname, searchParams } = parsed;
-    const res: YTUrl = { url: _url };
-
-    if (hostname.endsWith('youtu.be')) {
-      // youtu.be/video_id => video_id
-      res.videoId = pathname.slice(1);
-    } else if (hostname.split('.').includes('youtube')) {
-      res.videoId = searchParams.get('v') || undefined;
-    }
-    
-    res.playlistId = searchParams.get('list') || undefined;
-    res.index = Number.parseInt(searchParams.get('index')!) || undefined;
-    res.t = Number.parseInt(searchParams.get('t')!) || undefined;
-
-    url = res;
+    url = parseUrl(_url);
     dispatch('url', url);
   }
 </script>
---
+++ ui/src/lib/model/YTUrl.ts
@@ -0,0 +1,7 @@
+export interface YTUrl {
+	url: string;
+	videoId?: string;
+	playlistId?: string;
+	index?: number;
+	t?: number;
+}
