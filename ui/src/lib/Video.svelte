911fd23088e20e7dcab9bc2ab7dabb8c16b9315d
Adam Żbikowski <ajzbikowski@gmail.com>
2023-11-08 22:42:31 +0000

    Setup Video components for list & grid views

--- ui/src/lib/Video.svelte
+++ ui/src/lib/Video.svelte
@@ -9,41 +9,14 @@
 </script>
 
 <div class="flex gap-x-2">
-  <div class="rounded-sm aspect-[16/9] bg-cover bg-center" style="background-image: url('{PUBLIC_POCKETBASE_URL}/api/files/{collectionId}/{id}/{thumbnail}')">
-    <div class="w-48"></div>
+  <div class="rounded-sm aspect-[16/9] bg-contain bg-center w-48 h-fit flex-shrink-0 flex-grow-0" style="background-image: url('{PUBLIC_POCKETBASE_URL}/api/files/{collectionId}/{id}/{thumbnail}')">
   </div>
   <div class="">
-    <span class="inline-block font-semibold leading-snug mb-1">{title}</span>
-    <a href={expand?.channel?.handle_url} target="_blank" class="block hover:text-blue-700">{expand?.channel?.name}</a>
+    <span class="inline-block font-medium leading-snug mb-1.5">{title}</span>
+    <a href={expand?.channel?.handle_url} target="_blank" class="block text-sm hover:text-blue-700">{expand?.channel?.name}</a>
   </div>
 </div>
 
-<!-- <div class="entry">
-  <div class="thumbnail video-thumbnail" style="background-image: url('/{thumbnail}')">
-    <a href="/{video_id}" id="btn-video" class="btn-icon variant-filled">
-      <InfoIcon />
-    </a>
-  </div>
-  <div class="flex flex-col justify-start ml-2">
-    <div class="font-medium opacity-90">{title}</div>
-    <div class="text-gray-800 text-base">{channel || ''}</div>
-  </div>
-</div> -->
-
 <style lang="postcss">
-  .entry {
-		@apply flex py-1;
-	}
 
-  .video-thumbnail {
-    @apply h-16 grid place-items-center;
-  }
-
-  .video-thumbnail #btn-video {
-    @apply invisible opacity-0 transition-all duration-150 ease-in-out;
-  }
-
-  .video-thumbnail:hover #btn-video {
-    @apply visible opacity-100;
-  }
 </style>
---
+++ ui/src/lib/VideoListItem.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import InfoIcon from "@iconify-icons/material-symbols/info-rounded";
+	import Icon from "@iconify/svelte";
+
+  export let video: any;
+  $: ({ title, thumbnails, video_id, collectionId, id, channel } = video)
+</script>
+
+<div class="entry">
+  <div class="thumbnail video-thumbnail" style="background-image: url('{thumbnails[0].url}')">
+    <a href="/{video_id}" id="btn-video" class="btn-icon variant-filled">
+      <Icon icon={InfoIcon} />
+    </a>
+  </div>
+  <div class="flex flex-col justify-start ml-2">
+    <div class="font-medium opacity-90">{title}</div>
+    <div class="text-gray-800 text-base">{channel || ''}</div>
+  </div>
+</div>
+
+<style lang="postcss">
+  .entry {
+		@apply flex py-1;
+	}
+
+  .video-thumbnail {
+    @apply h-16 grid place-items-center;
+  }
+
+  .video-thumbnail #btn-video {
+    @apply invisible opacity-0 transition-all duration-150 ease-in-out;
+  }
+
+  .video-thumbnail:hover #btn-video {
+    @apply visible opacity-100;
+  }
+</style>
