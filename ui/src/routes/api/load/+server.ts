65d74474565e1a46959402adf1e8fc3f1e02c08f
Adam Żbikowski <ajzbikowski@gmail.com>
2023-11-16 22:53:39 +0000

    Rewrite for ASP.NET API

--- ui/src/routes/api/load/+server.ts
+++ ui/src/routes/api/load/+server.ts
@@ -1,9 +1,10 @@
+import { env } from '$env/dynamic/private';
 import { parseUrl } from '$lib/model/YTUrl.js';
 import { loadURL } from '$lib/youtube.js';
 
 const tag = '[api/load]';
 
-export async function POST({ fetch, params, request }) {
+/* export async function POST({ fetch, params, request }) {
 	const body = await request.json();
 	const ytUrl = parseUrl(<string>body.url);
 
@@ -16,6 +17,26 @@ export async function POST({ fetch, params, request }) {
 		return new Response(JSON.stringify(json), { status: 200 });
 	} catch (err) {
 		console.error(tag, 'failed to load json for url:', ytUrl);
+		return new Response(undefined, { status: 500 });
+	}
+} */
+
+export async function POST({ fetch, params, request }) {
+	const body = await request.json();
+	const ytUrl = parseUrl(<string>body.url);
+
+	if (!ytUrl || !ytUrl.videoId) {
+		return new Response('Invalid URL', { status: 400 });
+	}
+
+	try {
+		const url = new URL(env.API_URL + '/video/json');
+		url.searchParams.append('videoId', ytUrl.videoId);
+		const res = await fetch(url);
+		//const data = await res.json();
+		return new Response(res.body, { status: res.status });
+	} catch (err) {
+		console.error(tag, 'failed to load json for url:', ytUrl, err);
 		return new Response(undefined, { status: 500 });
 	}
 }
